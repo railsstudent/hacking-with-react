49b6daa2aeede046911b6350f47f1055f083681b
Connie Leung <cleung334@gmail.com>
2018-04-15 13:06:13 +0800

    Write first unit test in react

---
+++ src/pages/List.spec.js
@@ -0,0 +1,20 @@
+import React from "react";
+import TestUtils from "react-dom/test-utils";
+import { MemoryRouter } from "react-router-dom";
+
+import { describe, it, expect } from "../../tester/runner";
+
+const List = require("./List").default;
+
+// https://github.com/ReactTraining/react-router/blob/master/packages/react-router/docs/guides/testing.md
+describe("List", () => {
+  it("renders three repo links", () => {
+    const rendered = TestUtils.renderIntoDocument(
+      <MemoryRouter>
+        <List />
+      </MemoryRouter>
+    );
+    const repos = TestUtils.scryRenderedDOMComponentsWithTag(rendered, "li");
+    expect(repos.length).toEqual(3);
+  });
+});
